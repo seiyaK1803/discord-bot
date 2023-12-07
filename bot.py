8f310c61b0bd8e308a815d7b025ef8cf4772cea9
seiyak <seiyak@umich.edu>
2023-12-07 13:29:13 -0500

    created anonymous reactions. created slash commands, still need to add fields to them

--- bot.py
+++ bot.py
@@ -9,10 +9,17 @@ from config import EMBED_COLOR
 intents = discord.Intents.all()  # Use .all() to enable all intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+# define embed buttons
+class embedView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.message = None
+    
+    @discord.ui.button(label="Delete", style=discord.ButtonStyle.danger)
+    async def delete(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self.message.delete()
 
+# main reaction response
 @bot.event
 async def on_reaction_add(reaction, user):
     if reaction.emoji.id == EMOTE and user != bot.user:
@@ -25,11 +32,37 @@ async def on_reaction_add(reaction, user):
         embed.add_field(name="MSG Content", value=reaction.message.content, inline=False)
         embed.add_field(name="Go to Message", value=f"[Link to Message]({reaction.message.jump_url})", inline=False)
 
-        await channel.send(embed=embed)
+        view = embedView()
+        view.message = await channel.send(embed=embed, view=view)
+
+        # remove the user's reaction
+        await reaction.remove(user)
+        # react with the bot
+        await reaction.message.add_reaction(reaction.emoji)
 
 
+# slash commands
+@bot.tree.command(name='reaction_block', description='block a user from reaction')
+async def reactionBlock(ctx: discord.interactions.Interaction):
+    await ctx.response.send_message('hi')
+
+@bot.tree.command(name='reaction_unblock', description='unblock a user from reaction')
+async def reactionUnlock(ctx: discord.interactions.Interaction):
+    await ctx.response.send_message('hi2')
+
+@bot.tree.command(name='reaction_blocklist', description='list all blocked users')
+async def reactionBlocklist(ctx: discord.interactions.Interaction):
+    await ctx.response.send_message('hi3')
+
+# other commands (TODO delete these)
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.send('pong')
+
+# startup routine
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'Logged in as {bot.user.name}')
 
 bot.run(TOKEN)
